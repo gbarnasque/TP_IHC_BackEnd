dc45098a4a33bc87292039a3e75559f51638b8b8
Denyson Grellert <denysonjurgen@gmail.com>
2020-06-27 16:18:57 -0300

    Adicionar migrations
    Adiciona migrations que farão a base do banco de dados com os usuários e presenças

--- database/migrations/2020_06_27_145856_create_frequencies_table.php
+++ database/migrations/2020_06_27_145856_create_frequencies_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePessoa extends Migration
+class CreateFrequenciesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,13 @@ class CreatePessoa extends Migration
      */
     public function up()
     {
-        Schema::create('pessoa', function (Blueprint $table) {
+        Schema::create('frequencies', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->text('note')->nullable();
+            $table->unsignedBigInteger('student_id');
+
+            $table->foreign('student_id')->references('id')->on('users');
         });
     }
 
@@ -26,6 +30,6 @@ class CreatePessoa extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('pessoa');
+        Schema::dropIfExists('frequencies');
     }
 }
