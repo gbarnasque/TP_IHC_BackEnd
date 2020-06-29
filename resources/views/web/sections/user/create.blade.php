3fa0aadde3870a8fa6d1061f1e7f05497b84b04a
Denyson <denysonjurgen@gmail.com>
2020-06-29 19:19:11 -0300

    Corrigir botões radio
    Este commit corrige os botões radio de forma a serem mutualmente exclusivos. Também adiciona a propriedade name aos campos do form, de forma a serem acessíveis ao controller

--- resources/views/web/sections/user/create.blade.php
+++ resources/views/web/sections/user/create.blade.php
@@ -3,31 +3,32 @@
     <div class="col">
         <div>
             <form>
+                {{ csrf_field() }}
                 <div class="form-group form-div-create">
                     <label for="name">Nome <span style="color: red;">*</span></label>
-                    <input type="text" class="form-control">
+                    <input type="text" name="name" id="name" class="form-control">
 
                     <label for="mail">Email <span style="color: red;">*</span></label>
-                    <input type="email" id="mail" class="form-control">
+                    <input type="email" name="mail" id="mail" class="form-control">
 
                     <label for="password">Senha <span style="color: red;">*</span></label>
-                    <input type="password" id="password" class="form-control">
+                    <input type="password" name="password" id="password" class="form-control">
 
                     <label for="comment">Role<span style="color: red;">*</span></label>
                     <div class="form-check">
-                        <input type="radio" id="advisor" class="form-check-input" value="advisor">
+                        <input name="radio_person" type="radio" id="advisor" class="form-check-input" value="3">
                         <label class="form-check-label" for="advisor">
                             Orientador
                         </label>
                     </div>
                     <div class="form-check">
-                        <input type="radio" id="scholarship" class="form-check-input" value="scholarship">
+                        <input name="radio_person" type="radio" id="scholarship" class="form-check-input" value="2">
                         <label class="form-check-label" for="scholarship">
                             Bolsista
                         </label>
                     </div>
                     <div class="form-check">
-                        <input type="radio" id="admin" class="form-check-input" value="admin">
+                        <input name="radio_person" type="radio" id="admin" class="form-check-input" value="1">
                         <label class="form-check-label" for="admin">
                             Administrador
                         </label>
